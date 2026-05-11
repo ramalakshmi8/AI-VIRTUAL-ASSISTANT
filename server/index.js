e735d5bd23536dc6082ae8292a4a8076b16cea23
ramalakshmi8 <ramalakshumma482@gmail.com>
2026-05-11 23:03:37 +0530

    Update CORS origin for production environment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import geminiResponse from "./gemini.js";
 const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://assistantui.onrender.com", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
